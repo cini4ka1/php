21242b3af53f5c9396ac661f34af843ba8e8c32f
cini4ka1 <kkrakov71@gmail.com>
2025-11-22 14:14:06 +0300

    Update f_swap.php

--- lab2/f_swap.php
+++ lab2/f_swap.php
@@ -1,13 +1,32 @@
 <?php
 declare(strict_types=1);
 
-function swap(mixed &$x, mixed &$y): void
+function swap(&$a, &$b): void
 {
-    $temp = $x;
-    $x = $y;
-    $y = $temp;
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
 }
+?>
 
-$a = 5;
-$b = 8;
-swap($a, $b);
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Демонстрация функции swap</title>
+</head>
+<body>
+    <h1>Демонстрация функции swap</h1>
+    <?php
+    $a = 5;
+    $b = 8;
+    echo "До swap: a = $a, b = $b<br>";
+    swap($a, $b);
+    echo "После swap: a = $a, b = $b<br>";
+    echo "5 === \$b: " . (5 === $b ? 'true' : 'false') . "<br>";
+    echo "8 === \$a: " . (8 === $a ? 'true' : 'false') . "<br><br>";
+    ?> 
+</body>
+</html>
