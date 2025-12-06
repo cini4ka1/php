f43e6d67fa87dd4eaea18c0655e8ad49416d5202
cini4ka1 <kkrakov71@gmail.com>
2025-12-06 09:40:52 +0300

    Delete lab8/config.php

--- lab8/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-const DB_HOST = 'localhost';
-const DB_USER = 'f1184209_login_db';
-const DB_PASSWORD = 'nikitak968';
-const DB_NAME = 'f1184209_login_db';
-const DB_CHARSET = 'utf8mb4';
-?>
