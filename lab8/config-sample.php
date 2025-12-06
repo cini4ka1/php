6ba75e1e28892a222cc2cd82eba9b7e4287758d6
cini4ka1 <kkrakov71@gmail.com>
2025-12-06 08:47:55 +0300

    1

---
+++ lab8/config-sample.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+// Настройки подключения к базе данных
+define('DB_NAME', 'your_database_name');
+define('DB_USER', 'your_username');
+define('DB_PASSWORD', 'your_password');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8mb4');
+?>
