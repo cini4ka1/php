00774974a67cf06115d934552d0d8aa903bde07e
cini4ka1 <kkrakov71@gmail.com>
2025-12-07 18:55:07 +0300

    Update User.php

--- lab7/MyProject/Classes/User.php
+++ lab7/MyProject/Classes/User.php
@@ -1,10 +1,40 @@
 <?php
 namespace MyProject\Classes;
 
+/**
+ * Класс пользователя.
+ *
+ * Содержит основные данные о пользователе (имя, логин, пароль)
+ * и методы для получения информации и вывода её на экран.
+ */
+
 class User {
+        /**
+     * Имя пользователя.
+     *
+     * @var string
+     */
     public $name;
+        /**
+     * Логин пользователя.
+     *
+     * @var string
+     */
     public $login;
+        /**
+     * Пароль пользователя.
+     *
+     * @var string
+     */
     private $password;
+    
+    /**
+     * Конструктор пользователя.
+     *
+     * @param string $name     Имя пользователя.
+     * @param string $login    Логин пользователя.
+     * @param string $password Пароль пользователя.
+     */
 
     public function __construct(string $name, string $login, string $password) {
         $this->name = $name;
@@ -14,7 +44,10 @@ class User {
     }
 
     /**
-     * Выводит информацию о пользователе
+     * Выводит информацию о пользователе:
+     * имя, логин и пароль.
+     *
+     * @return void
      */
     public function showInfo(): void {
         echo "<div style='border: 1px solid #ccc; padding: 10px; margin: 10px;'>";
@@ -26,7 +59,11 @@ class User {
     }
 
     /**
-     * Деструктор класса User
+     * Деструктор пользователя.
+     *
+     * Выводит сообщение об удалении пользователя.
+     *
+     * @return void
      */
     public function __destruct() {
         echo "Пользователь {$this->login} удален<br>";
